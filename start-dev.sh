557dd30aeebd6e235e3f27b132fc5f186f62c044
Karyna Prokipchuk <karynaprokipchuk@gmail.com>
2025-11-04 15:30:05 +0200

    "Fix start-dev.sh"

--- start-dev.sh
+++ start-dev.sh
@@ -4,26 +4,33 @@
 ## Run application in DEV mode ##
 #################################
 
-
 started_at=$(date +"%s")
 
 echo "-----> Provisioning containers"
-docker compose --file docker-compose-dev.yaml up
+docker compose --file docker-compose-dev.yaml up -d
 echo ""
 
-# Run Sequalize's migrations.
+echo "-----> Waiting for server-dev to be ready..."
+until docker compose --file docker-compose-dev.yaml exec -T server-dev true &>/dev/null; do
+  printf "."
+  sleep 2
+done
+echo ""
+echo "Server-dev is ready!"
+
+
 echo "-----> Running application migrations"
-docker exec -it exam-project-server-dev-1 sequelize db:migrate
+docker compose --file docker-compose-dev.yaml exec server-dev npx sequelize-cli db:migrate
 echo ""
 
-# Run Sequalize's seeds.
 echo "-----> Running application seeds"
-docker exec -it exam-project-server-dev-1 sequelize db:seed:all
+docker compose --file docker-compose-dev.yaml exec server-dev npx sequelize-cli db:seed:all
 echo "<----- Seeds created"
+echo ""
 
 ended_at=$(date +"%s")
-
 minutes=$(((ended_at - started_at) / 60))
 seconds=$(((ended_at - started_at) % 60))
 
 echo "-----> Done in ${minutes}m${seconds}s"
+
